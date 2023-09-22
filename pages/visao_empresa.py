1ea78e1a0baf036d6a0ae0de2d8c3365003f0b9f
BETO MENDES <134823871+mendesbeto@users.noreply.github.com>
2023-09-22 15:24:47 -0300

    Update visao_empresa.py

--- pages/visao_empresa.py
+++ pages/visao_empresa.py
@@ -145,9 +145,9 @@ st.sidebar.markdown('''___''')
 st.sidebar.markdown('## Selecione uma das data limite')
 data_slider = st.sidebar.slider(
     'Até qual valor?',
-    value=pd.to_datetime (2022, 4, 13),
-    min_value=pd.to_datetime (2022, 2, 11),
-    max_value=pd.to_datetime (2022, 4, 6),
+    value= pd.datetime (2022, 4, 13),
+    min_value= pd.datetime (2022, 2, 11),
+    max_value= pd.datetime (2022, 4, 6),
     format='DD-MM-YYYY')
 
 st.sidebar.markdown('''---''')
